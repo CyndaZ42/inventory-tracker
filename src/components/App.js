b3aea5da05831698bb0aa36ca369a7f3e8f69f5e
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-09-25 22:16:55 -0700

    Add CoffeeControl

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import CoffeeControl from "./CoffeeControl";
+
+function App(){
+  return (
+    <React.Fragment>
+      <div className="container">
+        <Header />
+        <CoffeeControl />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/CoffeeControl.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class CoffeeControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainCoffeeList: [],
+      selectedCoffee: null,
+      editing: false
+    };
+  }
+}
+
+export default CoffeeControl;
