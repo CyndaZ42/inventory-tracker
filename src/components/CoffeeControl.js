b0a1f5dd51251713ecc4ca4285baf33df09e71ef
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-09-25 22:32:40 -0700

    Update CoffeeControl.js

--- src/components/CoffeeControl.js
+++ src/components/CoffeeControl.js
@@ -11,6 +11,32 @@ class CoffeeControl extends React.Component {
       editing: false
     };
   }
+
+  handleClick = () => {
+    if (this.state.selectedTea != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTea: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
+  
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null; 
+
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button> 
+      </React.Fragment>
+    );
+  }
 }
 
 export default CoffeeControl;
